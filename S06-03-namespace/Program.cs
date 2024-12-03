17deb314bf82ee6cb5b0d57da77b6a6347bb47b3
Mohsen Shahmohammadi <mohsen_sh5884@yahoo.com>
2024-12-03 16:56:19 -0500

    section6-03

---
+++ S06-03-namespace/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Domain.General;
+
+var student = new Person("Ali", "Rezaee");
+Console.WriteLine(student.FullName);
+
+
+
+namespace Domain.General
+{
+    class Person
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public string NationalCode;
+        public string FullName;
+
+        public Person(string fname, string lname)
+        {
+            FirstName = fname;
+            LastName = lname;
+            FullName = fname + " " + lname;
+        }
+    }
+}
