07f784e9a19094189ec1211da6c0e25e188c3c20
Mohsen Shahmohammadi <mohsen_sh5884@yahoo.com>
2024-12-03 16:10:49 -0500

    Section06-02

---
+++ S04-07-DatetimeType/Program.cs
@@ -0,0 +1,14 @@
+﻿var nowDate = "2024-12-03";
+var myDate2 = new DateTime(2024, 12, 03);
+var myDate3 = DateTime.Now;
+
+Console.WriteLine(nowDate);
+Console.WriteLine(myDate2);
+Console.WriteLine(myDate3);
+
+Console.WriteLine(myDate3.AddDays(6));
+Console.WriteLine(myDate3.DayOfWeek);
+Console.WriteLine(myDate3.Month);
+
+
+Console.WriteLine(myDate3.AddDays(6).DayOfWeek);
---
+++ S06-02-Class-Define/Program.cs
@@ -0,0 +1,30 @@
+﻿//var customer = new Person();
+
+//Console.WriteLine("My id is: "+customer.id);
+//Console.WriteLine("My id firstname: " + customer.firstName);
+
+//customer.firstName = "Ali";
+//customer.lastName = "rezaee";
+//Console.WriteLine("My id firstname: " + customer.firstName);
+
+var student = new Person("Ali", "Rezaee");
+Console.WriteLine(student.FirstName);
+Console.WriteLine(student.LastName);
+Console.WriteLine(student.FullName);
+
+
+class Person
+{
+    public int Id;
+    public string FirstName;
+    public string LastName;
+    public string NationalCode;
+    public string FullName;
+
+    public Person(string fname,string lname)
+    {
+        FirstName = fname;
+        LastName = lname;
+        FullName=fname+" "+ lname;
+    }
+}
