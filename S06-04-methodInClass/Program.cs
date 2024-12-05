48d4919d6c6113fcc84273cce674badd435e9416
Mohsen Shahmohammadi <mohsen_sh5884@yahoo.com>
2024-12-04 23:44:16 -0500

    S06-04 & S06-05 compete

---
+++ S06-04-methodInClass/Program.cs
@@ -0,0 +1,36 @@
+﻿Person person= new Person();
+person.Print("mohammad");
+person.Print("Ali", "1245544");
+
+
+
+class Person
+{
+    public int Id;
+    public string FirstName;
+    public string LastName;
+    public string NationalCode;
+
+    //public void Print(string name) => Console.WriteLine(name);
+
+    public void Print(string name,string nationalCode = "")
+    {
+        if (nationalCode == "")
+        {
+            Console.WriteLine(name);
+        }
+        else
+        {
+            Console.WriteLine("Name: "+name+" nationalCode: "+nationalCode);
+        }
+    }
+
+
+    string getPersonCode(string Id)
+    {
+        /*******  search ****/
+        var nationalCode = "00007666548";
+        Print("Ali");
+        return nationalCode;
+    }
+}
---
+++ S06-05-Constactor/Program.cs
@@ -0,0 +1,45 @@
+﻿Person person = new Person("Ali", "Rezaee");
+
+
+class Person
+{
+    public int Id;
+    public string FirstName;
+    public string LastName;
+    public string NationalCode;
+    private string FullName;
+
+    public Person()
+    {
+        FullName = FirstName + " " + LastName;
+    }
+
+    public Person(string firstName, string lastName)
+    {
+        FullName = firstName + " " + lastName;
+        Console.WriteLine("FullName= " + FullName);
+    }
+
+    public Person (string firsname,string lastname,string nationalCode)
+    {
+        Console.WriteLine("National code is:" + nationalCode);
+    }
+   
+}
+
+namespace Service
+{
+    class PersonService
+    {
+        public void Print(string name) => Console.WriteLine(name);
+
+
+        string getPersonCode(string Id)
+        {
+            /*******  search ****/
+            var nationalCode = "00007666548";
+            Print("Ali");
+            return nationalCode;
+        }
+    }
+}
