4499b8207156e0baa7eb2e02bcf522de0a210609
Mohsen Shahmohammadi <mohsen_sh5884@yahoo.com>
2024-12-05 14:21:04 -0500

    S06-07

---
+++ S06-07 readonly const var/Program.cs
@@ -0,0 +1,25 @@
+﻿CustomerCRM data3= new CustomerCRM("Ali",DateTime.Now,5);
+Console.WriteLine("name: "+data3.name);
+
+class CustomerCRM
+{
+    public readonly Guid id;
+    public readonly string name;
+    public readonly DateTime invoicedate;
+    private DateTime surveyDate;
+
+    public CustomerCRM(string name, DateTime invoiceDate, int duration)
+    {
+        id = Guid.NewGuid();
+        this.name = name;
+        this.invoicedate = invoiceDate;
+        this.surveyDate = invoiceDate.AddDays(duration);
+    }
+
+    public void PrintSurveyData(CustomerCRM data)
+    {
+        const int  number = 12;
+        surveyDate=invoicedate.AddDays(number);
+        Console.WriteLine("surveyDate: " + surveyDate);
+    }
+}
