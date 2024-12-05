8d61209c7f016541ff15588f4041189664d66254
Mohsen Shahmohammadi <mohsen_sh5884@yahoo.com>
2024-12-05 13:30:22 -0500

    S06-06 about this keyboard in method

---
+++ S06-06_thisInMethode/Program.cs
@@ -0,0 +1,38 @@
+﻿CustomerCRM data = new CustomerCRM("Ali", DateTime.Parse("2024-12-05"));
+
+data.PrintCRM(data);
+
+CustomerCRM data2 = new CustomerCRM("Reza", DateTime.Parse("2024-12-05"),DateTime.Parse("2024-12-20"));
+
+data.PrintCRM(data2);
+
+class CustomerCRM
+{
+    private Guid id;
+    private string name;
+    private DateTime invoicedate;
+    private DateTime surveyDate;
+
+    public CustomerCRM(string name,DateTime invoiceDate):this(name:name,invoiceDate:invoiceDate,7)
+    {
+    }
+    public CustomerCRM(string name, DateTime invoiceDate,int duration)
+    {
+        id = Guid.NewGuid();
+        this.name = name;
+        this.invoicedate = invoiceDate;
+        this.surveyDate = invoiceDate.AddDays(duration);
+    }
+    public CustomerCRM(string name, DateTime invoiceDate, DateTime surveyDate)
+    {
+        id = Guid.NewGuid();
+        this.name = name;
+        this.invoicedate = invoiceDate;
+        this.surveyDate = surveyDate;
+    }
+
+    public void PrintCRM(CustomerCRM customerCRM)
+    {
+        Console.WriteLine("Id: " + customerCRM.id + " Name: " + customerCRM.name + " surveyDate: " + customerCRM.surveyDate);
+    }
+}
