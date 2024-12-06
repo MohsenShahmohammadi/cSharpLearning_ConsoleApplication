883563ff55d2255c70dd2df5d4fc984c44e7161b
Mohsen Shahmohammadi <mohsen_sh5884@yahoo.com>
2024-12-06 15:39:05 -0500

    S06-08 Getter and Setter

---
+++ S06-08_getter_Setter/Program.cs
@@ -0,0 +1,36 @@
+﻿Person person = new Person("Ali", "Rezaee") {
+    Firstname = "Reza",
+    Lastname="Moradi"
+};
+
+Person person2 = new Person()
+{
+    Firstname = "Mohammad",
+    Lastname = "Hamidi"
+};
+Console.WriteLine(person.Fullname);
+
+Console.WriteLine(person.Desc);
+
+class Person
+{
+    public Guid id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; init; }
+    public string Fullname { get; private set; }
+    private DateTime createDate { get; set; }
+    public int age { get; }
+
+    public string Desc => "Your Fullname is: " + Firstname + " " + Lastname;
+
+    public Person()
+    {
+        Fullname = Firstname + " " + Lastname;
+    }
+    public Person(string fname,string lname)
+    {
+        Firstname = fname; 
+        Lastname = lname;
+        Fullname = Firstname + " " + Lastname;
+    }
+}
