89eace4d39bfa8a19b61af6a21b683ed90820331
Mohsen Shahmohammadi <mohsen_sh5884@yahoo.com>
2024-12-06 16:13:22 -0500

    S06-09 Static Class Method and Field

---
+++ S06-09_Static_Class/Program.cs
@@ -0,0 +1,41 @@
+﻿//Calculator.desc();
+
+//static class Calculator
+//{
+//    public static void desc()
+//    {
+//        Console.WriteLine("test");
+//    }
+//    public static void add(int numA, int numB)
+//    {
+//        Console.WriteLine($"{numA} + {numB} = {numA + numB}");
+//    }
+//     static void subtraction(int numA, int numB)
+//    {
+//        Console.WriteLine($"{numA} - {numB} = {numA - numB}");
+//    }
+//    static void multiPliction(int numA, int numB)
+//    {
+//        Console.WriteLine($"{numA} * {numB} = {numA + numB}");
+//    }
+//}
+
+class Setting
+{
+    public static string Name { get; set; }
+    public string Description { get; set; }
+    public string SMSProviderToken { get; set; }
+    public static int CountOfViewer { get; set; }
+    public Setting(string name, string desc, string token)
+    {
+        Name = name;
+        Description = desc;
+        SMSProviderToken = token;
+        CountOfViewer++;
+    }
+    static void getWebsiteInfo()
+    {
+        var result = "";
+        Console.WriteLine("Your Website's info is: " + result);
+    }
+}
