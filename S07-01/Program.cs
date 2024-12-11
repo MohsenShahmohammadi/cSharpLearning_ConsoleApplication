b29ef166302728a722675af8ef8e0642e547d819
Mohsen Shahmohammadi <mohsen_sh5884@yahoo.com>
2024-12-11 10:41:39 -0500

    S07-01

---
+++ S07-01/Program.cs
@@ -0,0 +1,26 @@
+﻿Employee emp = new Employee();
+emp.FirstName = "Reza";
+
+Customer customer = new Customer();
+customer.CustomerCode = 123;
+
+Person person = new Person();
+person.Id = 1;
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+public class Employee:Person
+{
+    public int EMPCode { get; set; }
+    public DateTime StartWorkDate { get; set; }
+}
+
+public class Customer : Person
+{
+    public int CustomerCode { get; set; }
+    public float MaxLimit { get; set; }
+}
