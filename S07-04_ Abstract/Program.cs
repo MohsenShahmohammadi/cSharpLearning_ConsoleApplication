5381b5af8027970b51e4ed44ff4cd77b671a888c
Mohsen Shahmohammadi <mohsen_sh5884@yahoo.com>
2024-12-19 12:31:52 -0500

    S07-04 - Abstract

---
+++ S07-04_ Abstract/Program.cs
@@ -0,0 +1,39 @@
+﻿//Animal animal=new Animal();
+Dog dog = new Dog(1,"Shitzo",50);
+dog.FullDescription();
+dog.MakeSound();
+
+public abstract class Animal
+{
+    public int Id { get; }
+    public string Name { get; }
+    public int AvrageLive { get; }
+    private DateTime createDate=DateTime.Now;
+
+    public Animal(int id,string name,int aveg)
+    {
+        Id = id;
+        Name = name;
+        AvrageLive = aveg;
+    }
+
+    public void FullDescription()
+    {
+        Console.WriteLine($"{Name} usually live about {AvrageLive} month(s)");
+    }
+    public abstract void MakeSound();
+
+}
+
+public class Dog : Animal
+{
+    public Dog(int id, string name, int aveg) : base(id, name, aveg)
+    {
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine("vagh vagh");
+    }
+}
+
