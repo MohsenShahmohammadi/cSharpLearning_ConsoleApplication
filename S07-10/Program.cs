3fd5c2da5e5026ae053ff67484491dc15a507ceb
Mohsen Shahmohammadi <mohsen_sh5884@yahoo.com>
2024-12-20 16:10:30 -0500

    S07-10

---
+++ S07-10/Program.cs
@@ -0,0 +1,38 @@
+﻿using MyProject;
+
+public class Token:IToken
+{
+    public string GetToken()
+    {
+        return Guid.NewGuid().ToString();
+    }
+}
+public class Info:IInfo
+{
+    public string GetInfo()
+    {
+        string FName = "Ali";
+        string LName = "Rezaee";
+        return FName + " " + LName;
+    }
+}
+
+public class EmailService:IToken,IInfo
+{
+    public string GetInfo()
+    {
+        string FName = "Ali";
+        string LName = "Rezaee";
+        return FName + " " + LName;
+    }
+
+    public string GetToken()
+    {
+        return Guid.NewGuid().ToString();
+    }
+
+    public void SendEmail(string name,string phone)
+    {
+        Console.WriteLine("Your Message Sent");
+    }
+}
