2ca3966701a7e4cefc31c34e4409febc2c11b886
Mohsen Shahmohammadi <mohsen_sh5884@yahoo.com>
2024-12-20 18:15:00 -0500

    S07-11

---
+++ S07-11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+
+Person person = new Person();
+person.Id = 1;
+person.FirstName = "Mohsen";
+person.LastName = "Shahmohammadi";
+
+var personJsoni=JsonSerializer.Serialize(person);
+Console.WriteLine(personJsoni);
+
+var serialized = "{\"Id\":1,\"FirstName\":\"Mohsen\",\"LastName\":\"Shahmohammadi\"}";
+
+var personUI = JsonSerializer.Deserialize<PersonUI>(serialized);
+
+Console.WriteLine(personUI.FirstName+" "+personUI.LastName);
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+class PersonUI
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
